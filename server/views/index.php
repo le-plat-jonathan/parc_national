5cbce643a2477be24f89304f4aec6f80b7c202cf
lsDevPlateforme <liron.saiman@laplateforme.io>
2024-09-16 11:16:50 +0200

    feature: Init_View

---
+++ server/views/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- IMPORTS ICONS-->
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="./src/css/styles.css" />
+        <title>Test PHP</title>        
+    </head>
+    <body>
+        <header class="header" id="home">
+            <div class="header__container container">
+
+            </div>
+        </header>
+        <main class="main">
+            <section class="section section__home">
+                <div class="home__container container">
+
+                </div>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+
+        <!-- JS -->
+        <script src="./src/js/main.js"></script>
+    </body>
+</html>
