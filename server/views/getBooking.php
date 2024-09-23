e4f1a7017ee139c6574c110cd682b91fdb0f281b
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-09-23 16:05:35 +0200

    js calendar

--- server/views/getBooking.php
+++ server/views/getBooking.php
@@ -1,15 +1,89 @@
+<?php
+// Vérifiez si le mois et l'année sont passés dans l'URL
+$currentMonth = isset($_GET['month']) ? (int)$_GET['month'] : (int)date('n'); // mois courant
+$currentYear = isset($_GET['year']) ? (int)$_GET['year'] : (int)date('Y'); // année courante
+
+// Gestion de l'incrémentation ou de la décrémentation
+if (isset($_GET['action'])) {
+    if ($_GET['action'] === 'addOne') {
+        $currentMonth++;
+        if ($currentMonth > 12) {
+            $currentMonth = 1;
+            $currentYear++;
+        }
+    } elseif ($_GET['action'] === 'lessOne') {
+        $currentMonth--;
+        if ($currentMonth < 1) {
+            $currentMonth = 12;
+            $currentYear--;
+        }
+    }
+}
+
+// Créez un objet DateTime pour le premier et le dernier jour du mois
+$dateFirstDayOfMonth = new DateTime("$currentYear-$currentMonth-01");
+$dateLastDayOfMonth = clone $dateFirstDayOfMonth;
+$dateLastDayOfMonth->modify('last day of this month');
+
+$dayFirstMonth = (int)$dateFirstDayOfMonth->format('N') % 7;
+$lastDayOfMonth = $dateLastDayOfMonth->format('d');
+
+$emptyDay = 0;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calendrier</title>
+    <link rel="stylesheet" href="/views/style.css">
+    <link rel="icon" href="data:,">
 </head>
 <body>
+    <form method="get" style="display:inline;">
+        <input type="hidden" name="month" value="<?php echo $currentMonth; ?>">
+        <input type="hidden" name="year" value="<?php echo $currentYear; ?>">
+        <button type="submit" name="action" value="lessOne">Moins un</button>
+    </form>
+    <form method="get" style="display:inline;">
+        <input type="hidden" name="month" value="<?php echo $currentMonth; ?>">
+        <input type="hidden" name="year" value="<?php echo $currentYear; ?>">
+        <button type="submit" name="action" value="addOne">Plus un</button>
+    </form>
 
-    <p>Je suis dans la vue</p>
-    <?php var_dump($data) ?>
-    
+    <table border="1">
+        <tr>
+            <th>Dim</th>
+            <th>Lun</th>
+            <th>Mar</th>
+            <th>Mer</th>
+            <th>Jeu</th>
+            <th>Ven</th>
+            <th>Sam</th>
+        </tr>
+        <tr>
+        <?php 
+        for($i = 0; $i < $dayFirstMonth; $i++){
+            echo "<th>vide</th>";
+            $emptyDay++;
+        }
+        
+        for($i = 1; $i <= $lastDayOfMonth; $i++){
+            $currentDay = $dateFirstDayOfMonth->format("Y-M-" . $i);
+            echo '<th id="' . $currentDay . '">' . $i . '</th>';
+            if (($i + $emptyDay) % 7 === 0){
+                echo '</tr><tr>';
+            }
+        }
+        ?>
+        </tr>
+    </table>
+    <script src="/views/script/calendar.js"></script>
 </body>
+<script>
+    const reservedDays = <?php echo json_encode($data); ?>;
+    localStorage.setItem('reservedDays', JSON.stringify(reservedDays));
+</script>
 </html>
--- server/views/script/calendar.js
+++ server/views/script/calendar.js
@@ -1,30 +1,3 @@
-btnLess = document.getElementById("lessOne")
-btnAdd = document.getElementById("addOne")
+const dates = JSON.parse(localStorage.getItem('reservedDays'));
+console.log(dates)
 
-uri = document.location.href
-
-function addOneToId() {
-    // Récupère l'URL actuelle
-    const currentUri = window.location.pathname;
-    
-    // Découpe l'URL en segments en utilisant "/" comme séparateur
-    const segments = currentUri.split('/');
-    
-    // Récupère le dernier segment
-    const lastSegment = segments.pop() || segments.pop(); // Pour gérer le "/" à la fin de l'URL
-    
-    console.log(segments)
-    console.log (parseInt(lastSegment)+1);
-
-    replace
-}
-
-addOneToId()
-
-// const id = getLastIdFromUri();
-
-// btnAdd.addEventListener('click', ()=> {
-//     newId = parseInt(id)
-//     document.location.href = uri + newId
-// })
-// console.log(uri)
