e6ee6182f152f7e6cd61d458e2d99c8901009a7d
jonathan-le-plat <jonathan.le-plat@laplateforme.io>
2024-09-24 11:09:14 +0200

    modified require of database in naturalRessources model & trail model

--- app/Models/NaturalRessources.php
+++ app/Models/NaturalRessources.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Database.php';
+require_once './../config/Database.php';
 
 class NaturalRessources extends Database{
 
--- app/Models/Trail.php
+++ app/Models/Trail.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Database.php';
+require_once './../config/Database.php';
 
 class Trail extends Database {
     protected $pdo;
