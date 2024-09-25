d515534962b08cfadba438413ab57658f4c6fefe
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-09-25 12:08:53 +0200

    update

--- app/views/NaturalRessources/updateNaturalRessources.php
+++ app/views/NaturalRessources/updateNaturalRessources.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Update a ressourcel</title>
+  <title>Update a ressource</title>
 </head>
 <body>
   <h1>Mise à jour des infos d'une ressource:</h1>
