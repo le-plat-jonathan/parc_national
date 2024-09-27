d9b2b82cf938c21ba85efd17c046a1b9e332268f
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-09-27 09:15:45 +0200

    update url for action

--- app/views/addPointOfInterest.php
+++ app/views/addPointOfInterest.php
@@ -6,16 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>addBungalow</title>
 </head>
+<?php
 
+ $uri = $_SERVER['REQUEST_URI'];
+ $scriptName = $_SERVER['SCRIPT_NAME'];
+ $urlForAction = str_replace('/views/addPointOfInterest.php', '/routes/pointOfInterestRoutes.php/createPointOfInterest', $uri);
+
+?>
 <body>
-    <form action="/routes/pointOfInterestRoutes.php/createPointOfInterest" method="post">
+    <form action="<?= $urlForAction ?>"  method="post">
         <label for="name">name</label>
         <input type="text" name="name" id="name">
 
-        <label for="longitude">description</label>
+        <label for="longitude">longitude</label>
         <input type="text" name="longitude" id="longitude">
 
-        <label for="latitude">price</label>
+        <label for="latitude">latitude</label>
         <input type="latitude" name="latitude" id="latitude">
 
         <input type="submit" value="post">
