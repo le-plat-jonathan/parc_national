4e551b42a47bf3e18b1a3b42f4315848aacf7bc4
jonathan-le-plat <jonathan.le-plat@laplateforme.io>
2024-10-02 14:25:30 +0200

    modified index.php -> removes 2 anchors unused

--- app/views/index.php
+++ app/views/index.php
@@ -33,7 +33,6 @@
         <div class="home__data">
           <span class="home__data-subtitle">Découvrez</span>
           <h1 class="home__data-title">Explorer <br> les plus belles <b>calanques <br> de Marseille.</b></h1>
-          <a href="#" class="button">Explorer</a>
 
         </div>
 
@@ -51,10 +50,7 @@
 
         <div class="home__info">
           <div>
-            <span class="home__info-title">5 meilleurs endroits à visiter</span>
-            <a href="" class="button button--flex button--link home__info-button">
-              More <i class="ri-arrow-right-line"></i>
-            </a>
+            <span class="home__info-title">Ces magnifiques destinations n'attendent que vous!</span>
           </div>
 
           <div class="home__info-overlay">
@@ -192,123 +188,6 @@
       </div>
     </section>
 
-    <!--==================== PLACES ====================-->
-    <!-- <section class="place section" id="place">
-      <h2 class="section__title">Choisissez votre place</h2>
-
-      <div class="place__container container grid">
-      
-        <div class="place__card">
-          <img src="src/img/place1.jpg" alt="" class="place__img">
-
-          <div class="place__content">
-            <span class="place__rating">
-              <i class="ri-star-line place__rating-icon"></i>
-              <span class="place__rating-number">4,8</span>
-            </span>
-
-            <div class="place__data">
-              <h3 class="place__title">Bali</h3>
-              <span class="place__subtitle">Indonesia</span>
-              <span class="place__price">$2499</span>
-            </div>
-          </div>
-
-          <button class="button button--flex place__button">
-            <i class="ri-arrow-right-line"></i>
-          </button>
-        </div>
-
-        
-        <div class="place__card">
-          <img src="src/img/place2.jpg" alt="" class="place__img">
-
-          <div class="place__content">
-            <span class="place__rating">
-              <i class="ri-star-line place__rating-icon"></i>
-              <span class="place__rating-number">5,0</span>
-            </span>
-
-            <div class="place__data">
-              <h3 class="place__title">Bora Bora</h3>
-              <span class="place__subtitle">Polinesia</span>
-              <span class="place__price">$1599</span>
-            </div>
-          </div>
-
-          <button class="button button--flex place__button">
-            <i class="ri-arrow-right-line"></i>
-          </button>
-        </div>
-
-        
-        <div class="place__card">
-          <img src="src/img/place3.jpg" alt="" class="place__img">
-
-          <div class="place__content">
-            <span class="place__rating">
-              <i class="ri-star-line place__rating-icon"></i>
-              <span class="place__rating-number">4,9</span>
-            </span>
-
-            <div class="place__data">
-              <h3 class="place__title">Hawaii</h3>
-              <span class="place__subtitle">EE.UU</span>
-              <span class="place__price">$3499</span>
-            </div>
-          </div>
-
-          <button class="button button--flex place__button">
-            <i class="ri-arrow-right-line"></i>
-          </button>
-        </div>
-
-      
-        <div class="place__card">
-          <img src="src/img/place4.jpg" alt="" class="place__img">
-
-          <div class="place__content">
-            <span class="place__rating">
-              <i class="ri-star-line place__rating-icon"></i>
-              <span class="place__rating-number">4,8</span>
-            </span>
-
-            <div class="place__data">
-              <h3 class="place__title">Whitehaven</h3>
-              <span class="place__subtitle">Australia</span>
-              <span class="place__price">$2499</span>
-            </div>
-          </div>
-
-          <button class="button button--flex place__button">
-            <i class="ri-arrow-right-line"></i>
-          </button>
-        </div>
-
-       
-        <div class="place__card">
-          <img src="src/img/place5.jpg" alt="" class="place__img">
-
-          <div class="place__content">
-            <span class="place__rating">
-              <i class="ri-star-line place__rating-icon"></i>
-              <span class="place__rating-number">4,8</span>
-            </span>
-
-            <div class="place__data">
-              <h3 class="place__title">Hvar</h3>
-              <span class="place__subtitle">Croacia</span>
-              <span class="place__price">$1999</span>
-            </div>
-          </div>
-
-          <button class="button button--flex place__button">
-            <i class="ri-arrow-right-line"></i>
-          </button>
-        </div>
-      </div>
-    </section> -->
-
     <!--==================== SUBSCRIBE ====================-->
     <section class="subscribe section">
       <div class="subscribe__bg">
@@ -341,12 +220,6 @@
         <div class="sponsor__content">
           <img src="src/img/sponsors3.png" alt="" class="sponsor__img">
         </div>
-        <!-- <div class="sponsor__content">
-          <img src="src/img/sponsors4.png" alt="" class="sponsor__img">
-        </div>
-        <div class="sponsor__content">
-          <img src="src/img/sponsors5.png" alt="" class="sponsor__img">
-        </div> -->
       </div>
     </section>
   </main>
