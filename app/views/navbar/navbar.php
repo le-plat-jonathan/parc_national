608ce3a8fe59b4a2475ec4835e8d147473ef8690
jonathan-le-plat <jonathan.le-plat@laplateforme.io>
2024-10-08 15:59:24 +0200

    fixed navbar

--- app/views/navbar/navbar.php
+++ app/views/navbar/navbar.php
@@ -12,6 +12,7 @@ if (strpos($_SERVER['HTTP_HOST'], 'localhost:8000') !== false) {
 } else {
     // WAMP
     $basePath = '/parc_national/app/routes/';
+    $basePathView = '/parc_national/app/views/';
 }
 
     $trail = $basePath . 'routeTrail.php/getAllTrail';
@@ -63,18 +64,14 @@ $endpoint = isset($urlParsed[0]) ? $urlParsed[0] : '';
                     echo '<li class="nav__item">';
                     echo '<a href="" class="nav__link">Mon compte</a>';
                     echo '</li>';
-                } elseif ($endpoint === '') {
-                    echo '<li class="nav__item">';
-                    echo '<a href="../../routes/userRoutes.php/get_user/' . $userId . '" class="nav__link">Mon compte</a>';
-                    echo '</li>';
                 } else {
                     echo '<li class="nav__item">';
-                    echo '<a href="./../userRoutes.php/get_user/' . $userId . '" class="nav__link">Mon compte</a>';
+                    echo '<a href=" ' . $basePath . 'userRoutes.php/get_user/' . $userId . '" class="nav__link">Mon compte</a>';
                     echo '</li>';
                 }
                 if($role === 'admin') {
                     echo '<li class="nav__item">';
-                    echo '<a href="../../../app/views/admin.php" class="nav__link">Administration</a>';
+                    echo '<a href=" ' . $basePathView . 'admin.php" class="nav__link">Administration</a>';
                     echo '</li>';
                 }
             }?>
