4d1e16a5498724d746afb9e1e598a714d17e7a81
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-10-09 16:19:32 +0200

    cleaning

--- app/Routes/userRoutes.php
+++ app/Routes/userRoutes.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . './../vendor/autoload.php';
-require_once  __DIR__ . './../Helpers/cors.php';
 require_once  __DIR__ . './../Controllers/UserController.php';
 
 if (session_status() == PHP_SESSION_NONE) {
--- app/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
-?>
